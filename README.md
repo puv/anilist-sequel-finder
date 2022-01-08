807f279ccfda06cc63a9d53bb8277340bbe8ebf7
Your Name <prfledev@gmail.com>
2022-01-08 13:56:05 +0200

    Initial commit for barebones setup.

---
+++ README.md
@@ -0,0 +1,4 @@
+# anilist-sequel-finder
+A small project that goes through your AniList account and finds sequels you have not watched
+
+[WIP] Probably doesan't work until this line is removed.
---
+++ index.js
@@ -0,0 +1,50 @@
+const request = require('request');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const domain = process.env.DOMAIN || 'http://localhost'; // include the http:// or https://
+const port = process.env.PORT || 3001;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+var options = {
+    uri: 'https://anilist.co/api/v2/oauth/token',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    json: {
+        'grant_type': 'authorization_code',
+        'client_id': '{client_id}',
+        'client_secret': '{client_secret}',
+        'redirect_uri': `${domain}:${port}/auth/callback`, // http://example.com/callback
+        'code': '{code}', // The Authorization Code received previously
+    }
+};
+
+app.all('/', (req, res) => {
+    res.redirect('/auth');
+});
+
+app.all('/auth', (req, res) => {
+    console.log('Authenticating...');
+    res.redirect(`https://anilist.co/api/v2/oauth/authorize?client_id=${client_id}&redirect_uri=` + `${domain}:${port}/auth/callback` + `&response_type=code`)
+});
+
+app.all('/auth/callback', (req, res) => {
+    try {
+        console.log('Authentication received...');
+        const AUTHCODE = req.query.code;
+        console.log("Auth Code: " + AUTHCODE);
+        res.sendStatus(200);
+    } catch {
+        console.log("Error authenticating");
+        res.sendStatus(401);
+
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+})
